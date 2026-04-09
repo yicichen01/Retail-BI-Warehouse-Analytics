6131eb08f4d8488c54fdb75eeb28aa03c4f04ff3
Yici Chen <yicichen@uw.edu>
2026-04-09 13:49:18 -0700

    Update language classification and clean up views structure

---
+++ sql/warehouse_views_index.sql
@@ -0,0 +1,14 @@
+-- SQL index for reporting-layer views
+
+-- Business question 1
+-- sql/views/vw_real_vs_target.sql
+-- sql/views/vw_store_product_sales.sql
+
+-- Business question 2
+-- sql/views/qry_bonus_basis.sql
+
+-- Business question 3
+-- sql/views/vw_store_sales_over_week.sql
+
+-- Business question 4
+-- sql/views/vw_num_of_store.sql
